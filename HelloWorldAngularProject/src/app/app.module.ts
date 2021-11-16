9b07890252c5b14a930e83b3fb9a7d28b306088a
ghoshdebashree <shree19ghosh@gmail.com>
2021-11-16 17:33:03 +0530

    Angular first project

---
+++ HelloWorldAngularProject/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TextcomponentComponent,
+    ImagecomponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
